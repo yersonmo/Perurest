b54ec3af988bb39926212c1431a1111dc81892f0
Enrique <e.villarroel@duocuc.cl>
2025-12-05 20:26:55 -0300

    Se comienza la aplicación desde cero para reemplazar el codigo por uno de una app que programé y probé en forma local.

--- Perurest/app/src/main/java/com/perurest/data/mapper/Mappers.kt
+++
@@ -1,9 +0,0 @@
-package com.perurest.data.mapper
-
-
-import com.perurest.data.local.DishEntity
-import com.perurest.domain.Dish
-
-
-fun DishEntity.toDomain() = Dish(id, name, description, price, imageUrl)
-fun Dish.toEntity() = DishEntity(id, name, description, price, imageUrl)
